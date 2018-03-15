22cf767c0a66ec7a637b8353860ac5f76acda1e7
LordHopkins <andrewhopkins0313@gmail.com>
2018-03-15 18:02:23 -0400

    Added a function that changes the language of the title of the page/ Adds the mailto: function for the contact us form

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ var Closebtn = document.getElementsByClassName('Closebtn')[0];
 var loginbtn = document.getElementById('submit-login');
 var signupbtn = document.getElementById('submit-signup');
 
+var frenchbtn = document.getElementById('language-chng');
+
 linkbtn.addEventListener('click', openModal);
 function openModal(){
   modal.style.display = 'block';
@@ -28,3 +30,11 @@ function closeModal() {
 
         $('#SimpleModal').hide();
       });
+
+
+  function chngText(){
+    document.getElementById('main-title').innerHTML='Le Lieu';
+}
+  function chngText_1() {
+    document.getElementById('main-title').innerHTML='The Place.';
+  }
